904a9ff75938a5c8f3b58a121dba1c72d371c164
Kwame Opare Asiedu <asieduoparekwame@gmail.com>
2023-12-24 21:22:55 +0000

    Added type declarations file

---
+++ dist/index.d.ts
@@ -0,0 +1,22 @@
+interface Route {
+  path: string | "*";
+  component: HTMLElement;
+}
+
+type Children = string | number | boolean | HTMLElement;
+
+export declare const Router: (
+  props: { routes: Route[] } & Partial<HTMLDivElement>,
+  ...children: Children[]
+) => HTMLElement;
+
+export declare const Link: (
+  props: { replace?: boolean } & Partial<HTMLAnchorElement>,
+  ...children: Children[]
+) => HTMLElement;
+
+export declare const getRouterPathname: () => string;
+
+export declare const getRouterParams: () => Record<string, string>;
+
+export declare const getRouterQuery: () => Record<string, string>;
--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,5 @@
 import van from 'vanjs-core';
 
-const routerState = van.state({
-    pathname: window.location.pathname,
-    params: {},
-    query: {}
-});
-function getRouterState() {
-    return routerState.val;
-}
-function updateRouterState(newState) {
-    routerState.val = Object.assign(Object.assign({}, routerState.val), newState);
-}
-
 /******************************************************************************
 Copyright (c) Microsoft Corporation.
 
@@ -46,25 +34,91 @@ typeof SuppressedError === "function" ? SuppressedError : function (error, suppr
     return e.name = "SuppressedError", e.error = error, e.suppressed = suppressed, e;
 };
 
+const _routerPathname = van.state(window.location.pathname);
+const _routerParams = van.state({});
+const _routerQuery = van.state({});
+
+const _QUERY_PARAM_REGEX$1 = /:([^\\d|^/]([^/]+)?)/;
 function Router(_a) {
     var { routes } = _a, props = __rest(_a, ["routes"]);
+    const ready = van.state(false);
     const rootElement = van.tags.div(Object.assign({}, props));
+    /** Match the current URL pathname to a route. Matching is done in the order of routes */
+    const routeMatcher = (path) => {
+        while (path !== "/" && path.endsWith("/")) {
+            path = path.slice(0, path.length - 1);
+        }
+        const pathParts = path.split("/");
+        const params = {};
+        let matchedRoute = null;
+        for (const route of routes) {
+            const routePathParts = route.path.split("/");
+            if (routePathParts.length !== pathParts.length)
+                continue;
+            let matchFound = true;
+            for (let idx = 0; idx < routePathParts.length; idx++) {
+                const routePathPart = routePathParts[idx];
+                const pathPart = pathParts[idx];
+                if (_QUERY_PARAM_REGEX$1.test(routePathPart)) {
+                    params[routePathPart.slice(1)] = pathPart;
+                }
+                else if (pathPart !== routePathPart) {
+                    matchFound = false;
+                    break;
+                }
+            }
+            if (matchFound) {
+                matchedRoute = route;
+                break;
+            }
+        }
+        if (!matchedRoute) {
+            // Find match-all (404) route if no match is found
+            matchedRoute = routes.find(route => route.path === "*") || null;
+        }
+        return { route: matchedRoute, params };
+    };
     const handleWindowPopState = () => {
-        const path = window.location.pathname;
-        const idx = routes.map(route => route.path).indexOf(path);
-        if (idx >= 0)
-            rootElement.replaceChildren(routes[idx].component);
+        const { route, params } = routeMatcher(window.location.pathname);
+        if (route) {
+            rootElement.replaceChildren(route.component);
+            _routerParams.val = params;
+        }
     };
     van.derive(() => {
         window.onpopstate = handleWindowPopState;
         handleWindowPopState();
     });
     van.derive(() => {
-        if (getRouterState().pathname) {
-            handleWindowPopState();
+        if (_routerPathname.val) {
+            if (ready.val) {
+                handleWindowPopState();
+            }
+            else
+                ready.val = true;
         }
     });
     return rootElement;
 }
 
-export { Router, getRouterState, updateRouterState };
+function Link(props, ...children) {
+    const _a = props, { onclick, href } = _a, rest = __rest(_a, ["onclick", "href"]);
+    const anchor = van.tags.a(Object.assign(Object.assign({}, rest), { href, onclick: (e) => {
+            var _a;
+            e.preventDefault();
+            window.history.pushState({}, "", href);
+            // Update the global state of the router to trigger the Router
+            if (href)
+                _routerPathname.val = href;
+            // Call original anchor onclick, if defined
+            (_a = onclick === null || onclick === void 0 ? void 0 : onclick.bind(anchor)) === null || _a === void 0 ? void 0 : _a(e);
+        } }), ...children);
+    return anchor;
+}
+
+const _QUERY_PARAM_REGEX = /:([^\\d|^/]([^/]+)?)/g;
+const getRouterPathname = () => _routerPathname.val;
+const getRouterParams = () => _routerParams.val;
+const getRouterQuery = () => _routerQuery.val;
+
+export { Link, Router, _QUERY_PARAM_REGEX, getRouterParams, getRouterPathname, getRouterQuery };
